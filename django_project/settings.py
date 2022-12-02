f7226c8bd170ad1f1d264d5ca379baade45873b3
tutorprogo123 <119608702+tutorprogo123@users.noreply.github.com>
2022-12-02 19:58:14 +0900

    fully update

--- django_project/settings.py
+++ django_project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-o4xlcj(-8c@qp-b2debk1*f(om!6b^or+@@mw#u*2c02t^_c97'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["https://turorial-production.up.railway.app/"]
 
 
 # Application definition
